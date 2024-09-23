99baaeed6cc5730597cf4ba11bbc75f79681fac3
Ido Gutman <idogut3@gmail.com>
2024-09-23 16:28:49 +0300

    created class Server and started adding the basic functions and functionality needed to each function ~idogut3

---
+++ Server.py
@@ -0,0 +1,45 @@
+import socket
+
+class Server:
+    def __init__(self, host):
+        DEFAULT_PORT = "1256"
+        port = DEFAULT_PORT
+        try:
+            port_info_file = open("port.info", "r")
+            # assuming the port_info_file is correct in its form
+            port = port_info_file.read()
+        except OSError as error:
+            print("Error: Error accessing port.info file")
+            print(error)
+            print("using default port: ", DEFAULT_PORT)
+        finally:
+            self.port = port
+            self.host = host
+            self.ADDR = (self.host, self.port)
+
+    def check_existing_database(self):  # Question 3
+        pass
+
+    def handle_client(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind(self.ADDR)
+            s.listen()
+            conn, addr = s.accept()
+            with conn:
+                print("connected by:", addr)
+                self.handle_connection(conn, addr)
+                # More code maybe here ...
+
+    def handle_connection(self, conn, addr):
+        pass
+
+    def run(self):
+        self.check_existing_database()
+        while True:
+            pass
+
+
+if __name__ == '__main__':
+    host = "127.0.0.1"
+    server = Server(host)
+    server.run()
