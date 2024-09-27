94f7221a26e17afda5ace2a77904f3210685e33a
Ido Gutman <idogut3@gmail.com>
2024-09-27 15:27:41 +0300

    added is_checksum_correct function ~idogut3

--- server/server_utils.py
+++ server/server_utils.py
@@ -3,3 +3,7 @@ from database.utils import checksum
 
 def calculate_checksum_value(file):
     return checksum.readfile(file)
+
+
+def is_checksum_correct(user_checksum, file) -> bool:
+    return user_checksum == calculate_checksum_value(file)
