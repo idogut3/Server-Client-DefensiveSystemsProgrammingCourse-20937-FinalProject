6a810b530e1dc5ff3eff5275b11fa25193553565
Ido Gutman <idogut3@gmail.com>
2024-09-28 18:17:06 +0300

    I decided to go with only one function in the reply and try to make the reply class only in charge of replying to the user and not receiving the user's replys ~idogut3

--- server/utils/Reply.py
+++ server/utils/Reply.py
@@ -1,12 +1,11 @@
 class Reply:
-    def __init__(self, reply_code):
+    def __init__(self, server_version, reply_code, payload = ""):
+        self.server_version = server_version
         self.reply_code = reply_code
+        self.payload = payload
+        self.payload_size = len(payload)
 
-    def respond(self):
+
+    def response(self):
         pass
 
-    def request(self):
-        pass
-
-    def send(self, file):
-        pass
