1a3460ed1731d48b297d1510f70324b3c9a903b5
Ido Gutman <idogut3@gmail.com>
2024-09-28 19:39:59 +0300

    created the Reply class that represents a reply that the server sends to the client ~idogut3

--- server/utils/Reply.py
+++ server/utils/Reply.py
@@ -1,11 +1,25 @@
+import struct
+
 class Reply:
-    def __init__(self, server_version, reply_code, payload = ""):
+    def __init__(self, server_version, reply_code, payload=""):
         self.server_version = server_version
         self.reply_code = reply_code
         self.payload = payload
         self.payload_size = len(payload)
 
+    def pack_message_header(self):
+        #struct format (That we pack the message header with):
+        # '<' represents we are using a Little-Endian Format
+        # 'B' represents an unsigned char (1 byte), 'H' represents unsigned short (2 bytes)
+        # 'H' represents Unsigned short (2 bytes)
+        # 'I' represents an unsigned int (4 bytes)
+        return struct.pack('<B H I', self.server_version, self.reply_code, self.payload_size)
 
-    def response(self):
-        pass
+    def response(self, conn):
+        message_header = self.pack_message_header()
+        message = pack_message_header_with_payload(message_header, self.payload)
+        conn.sendall(message)
 
+
+def pack_message_header_with_payload(message_header, message_payload):
+    return message_header + message_payload
