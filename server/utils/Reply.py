cf3e02e96b6bec4fdd123bca98313df41b6237f3
Ido Gutman <idogut3@gmail.com>
2024-09-28 20:37:48 +0300

    Decided that its better to divide The Reply class to a header and a payload, also structurally wise soooo I did ~idogut3

--- server/utils/Reply.py
+++ server/utils/Reply.py
@@ -1,25 +1,33 @@
 import struct
 
-class Reply:
-    def __init__(self, server_version, reply_code, payload=""):
+
+class Header:
+    def __init__(self, server_version, reply_code, payload_size):
         self.server_version = server_version
         self.reply_code = reply_code
-        self.payload = payload
-        self.payload_size = len(payload)
+        self.payload_size = payload_size
 
-    def pack_message_header(self):
-        #struct format (That we pack the message header with):
+    def pack_header(self):
+        # struct format (That we pack the message header with):
         # '<' represents we are using a Little-Endian Format
         # 'B' represents an unsigned char (1 byte), 'H' represents unsigned short (2 bytes)
         # 'H' represents Unsigned short (2 bytes)
         # 'I' represents an unsigned int (4 bytes)
         return struct.pack('<B H I', self.server_version, self.reply_code, self.payload_size)
 
+
+class Payload:
+    pass
+
+
+class Reply:
+    def __init__(self, header, payload=""):
+        self.header = header
+        self.payload = payload
+
+    def pack_message_header_with_payload(self):
+        return self.header + self.payload
+
     def response(self, conn):
-        message_header = self.pack_message_header()
-        message = pack_message_header_with_payload(message_header, self.payload)
+        message = self.pack_message_header_with_payload()
         conn.sendall(message)
-
-
-def pack_message_header_with_payload(message_header, message_payload):
-    return message_header + message_payload
