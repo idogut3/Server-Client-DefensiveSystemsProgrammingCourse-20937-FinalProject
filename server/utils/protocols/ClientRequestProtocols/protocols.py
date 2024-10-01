cefb506938f62719abd8b18c8bb205dad6e84779
Ido Gutman <idogut3@gmail.com>
2024-10-01 17:27:05 +0300

    Created an unpack unpack_send_file_payload function that unpacks the send file payload correctly will now move on to its protocol ~idogut3

--- server/utils/protocols/ClientRequestProtocols/protocols.py
+++ server/utils/protocols/ClientRequestProtocols/protocols.py
@@ -129,12 +129,31 @@ class RegisterRequestProtocol(Protocol):
         return Response(reply_header, packed_payload)
 
 
+def unpack_send_file_payload(payload):
+    # < is for little-endian format
+    # I: Unsigned 4-byte integer (Content Size, Original File Size)
+    # H: Unsigned 2-byte integer (Packet Number, Total Packets)
+    # 255s: File Name (255 bytes)
+    message_format = '<I I H H'
+
+    # Unpack first 12 bytes
+    content_size, original_file_size, packet_number, total_packets = struct.unpack(message_format, payload[:12])
+    # file_name is 255 bytes 255 + previous 12 bytes = 267
+    file_name = payload[12:267].decode('utf-8', 'ignore').rstrip('\x00')
+    message_content = payload[267:].decode('utf-8', 'ignore')
+
+    return content_size, original_file_size, packet_number, total_packets, file_name, message_content
+
+
 class SendFileRequestProtocol(Protocol):
     def __init__(self, server, conn):
         super().__init__(server, conn)
 
     def protocol(self, message):
-        pass
+        message_dict = unpack_message(message)
+        payload = message_dict["payload"]
+        content_size, original_file_size, packet_number, total_packets, file_name, message_content = \
+            unpack_send_file_payload(payload)
 
 
 class ReconnectToServerRequestProtocol(Protocol):
