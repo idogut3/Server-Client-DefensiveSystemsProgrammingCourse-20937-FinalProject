32ee9ff9b318a86eb54053ce85dc7b4db0e0a04e
Ido Gutman <idogut3@gmail.com>
2024-10-02 15:35:44 +0300

    added 3 function that work together get_header, get_full_message and unpack_message now everything is 'supposed to work' ~idogut3

--- server/utils/protocols/ClientRequestProtocols/protocols.py
+++ server/utils/protocols/ClientRequestProtocols/protocols.py
@@ -4,22 +4,7 @@ from abc import abstractmethod
 from server.utils.protocols.ClientRequestProtocols.Response import Response, Header
 from server.utils.protocols.codes.client_reply_codes_enum import ClientReplyCodes
 from server.utils.protocols.codes.server_reply_codes_enum import ServerReplyCodes
-
-
-def unpack_message(message):
-    # Extract fields according to the given structure
-    client_id = message[:16]  # 16 bytes for Client ID
-    version = message[16:17]  # 1 byte for Version
-    code = message[17:19]  # 2 bytes for Code
-    payload_size = struct.unpack("<I", message[19:23])[0]  # 4 bytes for Payload size (little-endian)
-    payload = message[23:23 + payload_size]  # Extract the payload of given size
-    return {
-        "client_id": client_id.decode('utf-8', 'ignore'),
-        "version": version.decode('utf-8', 'ignore'), #TODO: CHANGE TO UNPACK AS AN INTEGER
-        "code": int.from_bytes(code, 'little'),
-        "payload_size": payload_size,
-        "payload": payload.decode('utf-8', 'ignore')
-    }
+from server.utils.server_utils import unpack_message
 
 
 class Protocol:
@@ -39,7 +24,7 @@ class RegisterRequestProtocol(Protocol):
     def protocol(self, message) -> bool:
         message_dict = unpack_message(message)
         payload = message_dict["payload"]
-        username = payload[:255]
+        username = payload[:255].decode('utf-8')
 
         if not self.server.get_database().is_username_already_registered(username):
             accepted_register_request, uuid = self.accept_register_request(username)
@@ -177,7 +162,7 @@ class SendFileRequestProtocol(Protocol):
                               payload_size=payload_size)
         #TODO: add payload correctly AAAAAAAAAAAAAAA
         message_format = '<16s I 255s I'  # Format string: 16 bytes for Client ID, 4 bytes for Content Size, 255 bytes for File Name, 4 bytes for Cksum
-        packed_message = struct.pack(message_format, client_id, content_size, file_name_bytes, cksum)
+        # packed_message = struct.pack(message_format, client_id, content_size, file_name_bytes, cksum)
 
 
 class ReconnectToServerRequestProtocol(Protocol):
@@ -187,4 +172,3 @@ class ReconnectToServerRequestProtocol(Protocol):
     def protocol(self, message):
         pass
 
-# TODO<not really todo>: EXAMPLE FOR FUTURE REFERENCE: #reply = client_reply_protocols[ClientReplyCodes.REGISTER_REQUEST](server).protocol(message)
---
+++ server/utils/protocols/protocol_utils/send_file_protocol_utils.py
@@ -0,0 +1,5 @@
+from server.database.utils import checksum
+
+
+def calculate_checksum_value(file):
+    return checksum.readfile(file)
