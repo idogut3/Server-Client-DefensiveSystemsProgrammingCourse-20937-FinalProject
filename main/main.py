aa402b592a4a0b9e937ad159b48cde01950af628
idogut3 <idogut3@gmail.com>
2024-10-03 20:18:49 +0300

    TEST FOR WORKING IF IT WILL WORK ON THE PLANE ~idogut3

--- main/main.py
+++ main/main.py
@@ -1,7 +1,2 @@
-# if __name__ == '__main__':
-#     host = "127.0.0.1"
-    # server = Server(host)
-    # server.run()
-    #user_directory_name = 'users'
-    #os.makedirs(user_directory_name)
-    #user_folder = open("users\\rar1" , 'x+')
+if __name__ == '__main__':
+    print("OMG I RAR ALL DAY")
