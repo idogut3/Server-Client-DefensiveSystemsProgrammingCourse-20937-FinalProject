a6fa684c63a7bf13119caeb5362d9ecd76d39b1b
Ido Gutman <idogut3@gmail.com>
2024-10-14 20:13:12 +0300

    added to the utils hpp file

--- client-side/client-side/Header Files/utils.hpp
+++ client-side/client-side/Header Files/utils.hpp
@@ -1,5 +1,16 @@
 #ifndef UTILS_HPP
 #define UTILS_HPP
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <boost/asio.hpp>
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/nil_generator.hpp>
+#include <boost/uuid/uuid_io.hpp>
+#include <boost/endian/conversion.hpp>
+#include <string.h>
 
+
+constexpr auto VERSION = 3;
 
 #endif
