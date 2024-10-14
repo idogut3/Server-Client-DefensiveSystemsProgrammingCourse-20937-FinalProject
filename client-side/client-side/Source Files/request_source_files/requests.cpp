e95a60492dc36274041ad9267491cdcffb508d0c
Ido Gutman <idogut3@gmail.com>
2024-10-14 20:40:40 +0300

    added the implemention of the abstract function getPayload in the requests.cpp file of each class ~idogut3

--- client-side/client-side/Source Files/request_source_files/requests.cpp
+++ client-side/client-side/Source Files/request_source_files/requests.cpp
@@ -1,1 +1,49 @@
 #include "Header Files\request_header_files\requests.h"
+
+RegisterRequest::RegisterRequest(RequestHeader header, RegistrationPayload payload)
+    : Request(header), payload(payload) {}
+
+Payload RegisterRequest::getPayload() const {
+    return payload; 
+}
+
+SendPublicKeyRequest::SendPublicKeyRequest(RequestHeader header, SendPublicKeyPayload payload)
+    : Request(header), payload(payload) {}
+
+Payload SendPublicKeyRequest::getPayload() const {
+    return payload; 
+}
+
+ReconnectRequest::ReconnectRequest(RequestHeader header, ReconnectionPayload payload)
+    : Request(header), payload(payload) {}
+
+Payload ReconnectRequest::getPayload() const {
+    return payload; 
+
+ValidCrcRequest::ValidCrcRequest(RequestHeader header, ValidCrcPayload payload)
+    : Request(header), payload(payload) {}
+
+Payload ValidCrcRequest::getPayload() const {
+    return payload; 
+}
+
+InvalidCrcRequest::InvalidCrcRequest(RequestHeader header, InvalidCrcPayload payload)
+    : Request(header), payload(payload) {}
+
+Payload InvalidCrcRequest::getPayload() const {
+    return payload; 
+}
+
+InvalidCrcDoneRequest::InvalidCrcDoneRequest(RequestHeader header, InvalidCrcDonePayload payload)
+    : Request(header), payload(payload) {}
+
+Payload InvalidCrcDoneRequest::getPayload() const {
+    return payload; 
+}
+
+SendFileRequset::SendFileRequset(RequestHeader header, SendFilePayload payload)
+    : Request(header), payload(payload) {}
+Payload SendFileRequset::getPayload() const {
+    return payload; // Return the payload
+}
+
