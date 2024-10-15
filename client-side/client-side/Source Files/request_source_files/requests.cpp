953cdfe7f0de3d95ad218c44873df1faa3d1050a
Ido Gutman <idogut3@gmail.com>
2024-10-15 13:00:54 +0300

    fomated code ~idogut3

--- client-side/client-side/Source Files/request_source_files/requests.cpp
+++ client-side/client-side/Source Files/request_source_files/requests.cpp
@@ -7,12 +7,16 @@ Payload RegisterRequest::getPayload() const {
 	return payload;
 }
 
+
+
 SendPublicKeyRequest::SendPublicKeyRequest(RequestHeader header, SendPublicKeyPayload payload)
 	: Request(header), SendPublicKeyPayload(payload) {}
 
 Payload SendPublicKeyRequest::getPayload() const {
 	return payload;
 }
+
+
 
 ReconnectRequest::ReconnectRequest(RequestHeader header, ReconnectionPayload payload)
 	: Request(header), ReconnectionPayload(payload) {}
@@ -21,6 +25,8 @@ Payload ReconnectRequest::getPayload() const {
 	return payload;
 }
 
+
+
 ValidCrcRequest::ValidCrcRequest(RequestHeader header, ValidCrcPayload payload)
 	: Request(header), ValidCrcPayload(payload) {}
 
@@ -28,24 +34,30 @@ ValidCrcRequest::ValidCrcRequest(RequestHeader header, ValidCrcPayload payload)
 		return payload;
 	}
 
-	InvalidCrcRequest::InvalidCrcRequest(RequestHeader header, InvalidCrcPayload payload)
-		: Request(header), InvalidCrcPayload(payload) {}
 
-	Payload InvalidCrcRequest::getPayload() const {
-		return payload;
-	}
 
-	InvalidCrcDoneRequest::InvalidCrcDoneRequest(RequestHeader header, InvalidCrcDonePayload payload)
-		: Request(header), InvalidCrcDonePayload(payload) {}
+InvalidCrcRequest::InvalidCrcRequest(RequestHeader header, InvalidCrcPayload payload)
+	: Request(header), InvalidCrcPayload(payload) {}
 
-	Payload InvalidCrcDoneRequest::getPayload() const {
-		return payload;
-	}
+Payload InvalidCrcRequest::getPayload() const {
+	return payload;
+}
 
-	SendFileRequest::SendFileRequest(RequestHeader header, SendFilePayload payload)
-		: Request(header), SendFilePayload(payload) {}
 
-	Payload SendFileRequest::getPayload() const {
-		return payload; // Return the payload
-	}
+
+InvalidCrcDoneRequest::InvalidCrcDoneRequest(RequestHeader header, InvalidCrcDonePayload payload)
+	: Request(header), InvalidCrcDonePayload(payload) {}
+
+Payload InvalidCrcDoneRequest::getPayload() const {
+	return payload;
+}
+
+
+
+SendFileRequest::SendFileRequest(RequestHeader header, SendFilePayload payload)
+	: Request(header), SendFilePayload(payload) {}
+
+Payload SendFileRequest::getPayload() const {
+	return payload; // Return the payload
+}
 
